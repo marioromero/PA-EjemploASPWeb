a167b2f3708c2270a03a1191deaa7ced6e2769ba
MarioRomeroTTP <mario.romero@totalpack.cl>
2024-10-13 22:48:27 -0300

    Modelo agregado

---
+++ EjemploASPWeb/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace EjemploASPWeb.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public DateTime Genero { get; set; }
+        public string FechaNacimiento { get; set; }
+        public int NumeroDeMascotas { get; set; }
+    }
+}
